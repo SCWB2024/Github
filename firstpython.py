61d592a3447d81c225e7ec99487cc28b3f85a738
Sandra White-Belgrave <155330836+SCWB2024@users.noreply.github.com>
2026-01-20 12:38:56 -0400

    Add first Python script with greeting functionality

---
+++ firstpython.py
@@ -0,0 +1,6 @@
+# firstpython.py
+# This is my first Python file on GitHub
+
+print("Hello! Welcome to my GitHub Python project.")
+name = input("What is your name? ")
+print("Hello,", name, "! Have a great day.")
